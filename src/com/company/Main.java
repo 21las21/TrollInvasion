9f2256df8fef443ee887f29b63ee72dd1cb7cc7e
21las21 <sasha.kudasov04@gmail.com>
2017-11-01 21:23:21 +0500

    - Added second phase to the game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         String read = scanner.nextLine();
         while (!Objects.equals(read, "Starting game")) {
             Player player = null;
-            String name;
+            String name = null;
             if (read.startsWith("+")) {
                 name = read.substring(1);
                 for (Player player1 : players) {
@@ -69,81 +69,156 @@ public class Main {
         cells[1][3].player = players.get(0);
         cells[1][3].unit = 2;
         cells[3][3].player = players.get(1);
-        cells[3][3].unit = 2;
+        cells[3][3].unit = 3;
 
+        //Map building
+        mapBuild(cells);
+        //Game loop start
         int playerTurn = 0;
         while (true) {
-            for (Cell[] cells1 : cells) {
-                for (Cell cell : cells1) {
-                    if (cell == null) {
-                        System.out.print("__ ");
-                    } else if (cell.player == null) {
-                        System.out.print("## ");
-                    } else {
-                        System.out.print(cell.unit);
-                        System.out.print(cell.player.name);
-                        System.out.print(" ");
+            //First phase start
+            while (true) {
+                System.out.print(players.get(playerTurn).name);
+                System.out.println("'s turn");
+                int cordI;
+                int cordJ;
+                Cell selectedCell;
+                boolean good = false;
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        if (cells[i][j] != null && cells[i][j].player == players.get(playerTurn) && cells[i][j].unit != 1) {
+                            good = true;
+                        }
                     }
                 }
-                System.out.println();
+                if (!good) {
+                    break;
+                }
+                while (true) {
+                    String[] select = null;
+                    String line;
+                    while (true) {
+                        line = scanner.nextLine();
+                        if (line.startsWith(players.get(playerTurn).name + ":")) {
+                            break;
+                        }
+                    }
+                    select = line.substring(2).split(" ");
+                    cordI = Integer.parseInt(select[0]);
+                    cordJ = Integer.parseInt(select[1]);
+                    selectedCell = cells[cordI][cordJ];
+                    if (selectedCell.player != players.get(playerTurn) || selectedCell.unit < 2) {
+                        System.out.println("Invalid choice!");
+                    } else {
+                        break;
+                    }
+                }
+                ArrayList<Cell> nearCells = new ArrayList<>();
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        int dI = Math.abs(cordI - i);
+                        int dJ = Math.abs(cordJ - j);
+                        if ((dI == 2 && dJ == 0) || (dI == 1 && dJ == 1)) {
+                            nearCells.add(cells[i][j]);
+                        }
+                    }
+                }
+                Cell goCell;
+                while (true) {
+                    String[] go;
+                    String line;
+                    while (true) {
+                        line = scanner.nextLine();
+                        if (line.startsWith(players.get(playerTurn).name + ":")) {
+                            break;
+                        }
+                    }
+                    go = line.substring(2).split(" ");
+                    goCell = cells[Integer.parseInt(go[0])][Integer.parseInt(go[1])];
+                    if (!nearCells.contains(goCell)) {
+                        System.out.println("Invalid choice!");
+                    } else {
+                        break;
+                    }
+                }
+                if (goCell.player == null) {
+                    goCell.player = players.get(playerTurn);
+                    goCell.unit = selectedCell.unit - 1;
+                    selectedCell.unit = 1;
+                } else {
+                    if (selectedCell.unit > goCell.unit) {
+                        goCell.player = players.get(playerTurn);
+                    }
+                    goCell.unit = Math.abs(selectedCell.unit - goCell.unit);
+                    if (goCell.unit == 0) {
+                        goCell.unit = 1;
+                    }
+                    selectedCell.unit = 1;
+                }
+                mapBuild(cells);
             }
+            //First phase end
 
-            System.out.print(players.get(playerTurn).name);
-            System.out.println("'s turn");
-            int cordI;
-            int cordJ;
-            Cell selectedCell;
+            System.out.println("Upgrade phase");
+            //mapBuild(cells);
+
+            //Second phase start
+            int energy = 0;
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 7; j++) {
+                    if (cells[i][j] != null && cells[i][j].player == players.get(playerTurn)) {
+                        energy++;
+                    }
+                }
+            }
+            System.out.println("You have " + energy + " energy left");
+            Cell upgradeCell;
             while (true) {
-                String[] select = scanner.nextLine().split(" ");
-                cordI = Integer.parseInt(select[0]);
-                cordJ = Integer.parseInt(select[1]);
-                selectedCell = cells[cordI][cordJ];
-                if (selectedCell.player != players.get(playerTurn) || selectedCell.unit < 2) {
+                String[] upgrade;
+                String line;
+                while (true) {
+                    line = scanner.nextLine();
+                    if (line.startsWith(players.get(playerTurn).name + ":")) {
+                        break;
+                    }
+                }
+                upgrade = line.substring(2).split(" ");
+                upgradeCell = cells[Integer.parseInt(upgrade[0])][Integer.parseInt(upgrade[1])];
+                if (upgradeCell.player != players.get(playerTurn)) {
                     System.out.println("Invalid choice!");
                 }
                 else {
+                    energy--;
+                    upgradeCell.unit++;
+                    mapBuild(cells);
+                    System.out.println(energy + " energy left");
+                }
+                if (energy == 0) {
                     break;
                 }
-            }
-            ArrayList<Cell> nearCells = new ArrayList<>();
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 7; j++) {
-                    int dI = Math.abs(cordI - i);
-                    int dJ = Math.abs(cordJ - j);
-                    if ((dI == 2 && dJ == 0) || (dI == 1 && dJ == 1)) {
-                        nearCells.add(cells[i][j]);
-                    }
-                }
-            }
-            Cell goCell;
-            while (true) {
-                String[] go = scanner.nextLine().split(" ");
-                goCell = cells[Integer.parseInt(go[0])][Integer.parseInt(go[1])];
-                if (!nearCells.contains(goCell)) {
-                    System.out.println("Invalid choice!");
-                } else {
-                    break;
-                }
-            }
-            if (goCell.player == null) {
-                goCell.player = players.get(playerTurn);
-                goCell.unit = selectedCell.unit - 1;
-                selectedCell.unit = 1;
-            } else {
-                if (selectedCell.unit > goCell.unit) {
-                    goCell.player = players.get(playerTurn);
-                }
-                goCell.unit = Math.abs(selectedCell.unit - goCell.unit);
-                if (goCell.unit == 0) {
-                    goCell.unit = 1;
-                }
-                selectedCell.unit = 1;
             }
 
             playerTurn++;
             if (playerTurn >= players.size()) {
                 playerTurn = 0;
             }
+        }
+    }
+
+    public static void mapBuild(Cell[][] cells) {
+        for (Cell[] cells1 : cells) {
+            for (Cell cell : cells1) {
+                if (cell == null) {
+                    System.out.print("__ ");
+                } else if (cell.player == null) {
+                    System.out.print("## ");
+                } else {
+                    System.out.print(cell.unit);
+                    System.out.print(cell.player.name);
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
